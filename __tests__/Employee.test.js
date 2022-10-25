9df708c061a44955e9fbfd11ace0db8982795544
Peter Kim <peter.kim@uconn.edu>
2022-10-25 07:59:15 -0400

    Added module.exports to employee classes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,44 +1,52 @@
 const Employee = require("../lib/Employee");
-const employee = new Employee("Peter", 89, "test@gmail.com");
 
-describe ("Employee class", () => {
-    it("Creates a new Employee object", () => {        
-        expect(typeof(employee)).toBe("object");
-    });
 
-    it("Assigns the employee a name", () => {
-        expect(employee.name).toBe("Peter");
-    });
+describe("Employee class", () => {
+	it("Creates a new Employee object", () => {
+        const employee = new Employee("Peter", 89, "test@gmail.com");
+		expect(typeof(employee)).toBe("object");
+	});
 
-    it("Assigns the employee an id number", () => {
-        expect(employee.id).toBe(89);
-    });
+	it("Assigns the employee a name", () => {
+        const employee = new Employee("Peter", 89, "test@gmail.com");
+		expect(employee.name).toBe("Peter");
+	});
 
-    it("Assigns the employee an email address", () => {
-        expect(employee.email).toBe("test@gmail.com");
-    });
-});
+	it("Assigns the employee an id number", () => {
+        const employee = new Employee("Peter", 89, "test@gmail.com");
+		expect(employee.id).toBe(89);
+	});
 
-describe ("getName()", () => {
-    it("Returns the employee's name", () => {
-        expect(employee.getName()).toBe("Peter");
-    })
-});
+	it("Assigns the employee an email address", () => {
+        const employee = new Employee("Peter", 89, "test@gmail.com");
+		expect(employee.email).toBe("test@gmail.com");
+	});
 
-describe ("getId()", () => {
-    it("Returns the employee's id number", () => {
-        expect(employee.getId()).toBe(89);
-    })
-});
+	describe("getName()", () => {
+		it("Returns the employee's name", () => {
+            const employee = new Employee("Peter", 89, "test@gmail.com");
+			expect(employee.getName()).toBe("Peter");
+		});
+	});
 
-describe ("getEmail()", () => {
-    it("Returns the employee's email address", () => {
-        expect(employee.getEmail()).toBe("test@gmail.com");
-    })
-});
+	describe("getId()", () => {
+		it("Returns the employee's id number", () => {
+            const employee = new Employee("Peter", 89, "test@gmail.com");
+			expect(employee.getId()).toBe(89);
+		});
+	});
 
-describe ("getRole()", () => {
-    it("Returns 'Employee'", () => {
-        expect(employee.getRole()).toBe("Employee");
-    })
+	describe("getEmail()", () => {
+		it("Returns the employee's email address", () => {
+            const employee = new Employee("Peter", 89, "test@gmail.com");
+			expect(employee.getEmail()).toBe("test@gmail.com");
+		});
+	});
+
+	describe("getRole()", () => {
+		it("Returns 'Employee'", () => {
+            const employee = new Employee("Peter", 89, "test@gmail.com");
+			expect(employee.getRole()).toBe("Employee");
+		});
+	});
 });
