6481fd2b6383fdf7224a472cb90baa6542e980a7
Peter Kim <peter.kim@uconn.edu>
2022-10-25 08:12:19 -0400

    Engineer.test.js tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,51 +1,44 @@
 const Employee = require("../lib/Employee");
-
+const employee = new Employee("Peter", 89, "test@gmail.com");
 
 describe("Employee class", () => {
 	it("Creates a new Employee object", () => {
-        const employee = new Employee("Peter", 89, "test@gmail.com");
 		expect(typeof(employee)).toBe("object");
 	});
 
 	it("Assigns the employee a name", () => {
-        const employee = new Employee("Peter", 89, "test@gmail.com");
+       
 		expect(employee.name).toBe("Peter");
 	});
 
 	it("Assigns the employee an id number", () => {
-        const employee = new Employee("Peter", 89, "test@gmail.com");
 		expect(employee.id).toBe(89);
 	});
 
 	it("Assigns the employee an email address", () => {
-        const employee = new Employee("Peter", 89, "test@gmail.com");
 		expect(employee.email).toBe("test@gmail.com");
 	});
 
 	describe("getName()", () => {
 		it("Returns the employee's name", () => {
-            const employee = new Employee("Peter", 89, "test@gmail.com");
 			expect(employee.getName()).toBe("Peter");
 		});
 	});
 
 	describe("getId()", () => {
 		it("Returns the employee's id number", () => {
-            const employee = new Employee("Peter", 89, "test@gmail.com");
 			expect(employee.getId()).toBe(89);
 		});
 	});
 
 	describe("getEmail()", () => {
 		it("Returns the employee's email address", () => {
-            const employee = new Employee("Peter", 89, "test@gmail.com");
 			expect(employee.getEmail()).toBe("test@gmail.com");
 		});
 	});
 
 	describe("getRole()", () => {
 		it("Returns 'Employee'", () => {
-            const employee = new Employee("Peter", 89, "test@gmail.com");
 			expect(employee.getRole()).toBe("Employee");
 		});
 	});
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,54 @@
+const Engineer = require("../lib/Engineer");
+const engineer = new Engineer("Bob", 12, "test@gmail.com", "BobGitHub");
+
+describe("Engineer class", () => {
+    it("Creates a new Engineer object", () => {
+        expect(typeof(engineer)).toBe("object");
+    });
+
+    it("Assigns the engineer a name", () => {
+        expect(engineer.name).toBe("Bob");
+    });
+
+    it("Assigns the engineer an id number", () => {
+        expect(engineer.id).toBe(12);
+    });
+    
+    it("Assigns the engineer an email", () => {
+        expect(engineer.email).toBe("test@gmail.com");
+    });
+
+    it("Assigns the engineer a github username", () => {
+        expect(engineer.github).toBe("BobGitHub");
+    });
+
+    describe("getName()", () => {
+		it("Returns the engineer's name", () => {
+			expect(engineer.getName()).toBe("Bob");
+		});
+	});
+
+	describe("getId()", () => {
+		it("Returns the engineer's id number", () => {
+			expect(engineer.getId()).toBe(12);
+		});
+	});
+
+	describe("getEmail()", () => {
+		it("Returns the employee's email address", () => {
+			expect(engineer.getEmail()).toBe("test@gmail.com");
+		});
+	});
+
+    describe("getGithub()", () => {
+        it("Returns the engineer's github username", () => {
+            expect(engineer.getGithub()).toBe("BobGitHub");
+        });
+    });
+
+    describe("getRole()", () => {
+        it("Returns 'Engineer'", () => {
+            expect(engineer.getRole()).toBe("Engineer");
+        });
+    });
+});
